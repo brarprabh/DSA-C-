baa6814c684e27b58c23f68c46e63b56a05623b2
brarprabh <prabhjotsinghbrar39@gmail.com>
2025-08-20 11:29:36 +0530

    add reverse pairs

--- src/hello.cpp
+++ src/hello.cpp
@@ -2144,10 +2144,82 @@
 
 // Count the number of inversions
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int merge(vector<int> &arr, int n, int low, int mid, int high)
+// {
+//     vector<int> temp;
+//     int left = low;
+//     int right = mid + 1;
+//     int count = 0;
+//     while (left <= mid && right <= high)
+//     {
+//         if (arr[left] <= arr[right])
+//         {
+//             temp.push_back(arr[left]);
+//             left++;
+//         }
+//         else
+//         {
+//             temp.push_back(arr[right]);
+//             count += (mid - left + 1);
+//             right++;
+//         }
+//     }
+//     while (left <= mid)
+//     {
+//         temp.push_back(arr[left]);
+//         left++;
+//     }
+//     while (right <= high)
+//     {
+//         temp.push_back(arr[right]);
+//         right++;
+//     }
+//     for (int i = low; i <= high; i++)
+//     {
+//         arr[i] = temp[i - low];
+//     }
+//     return count;
+// }
+
+// int mergesort(vector<int> &arr, int n, int low, int high)
+// {
+//     int count = 0;
+//     int mid = (low + high) / 2;
+
+//     if (low == high)
+//         return count;
+//     count += mergesort(arr, n, low, mid);
+//     count += mergesort(arr, n, mid + 1, high);
+//     count += merge(arr, n, low, mid, high);
+//     return count;
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<int> arr;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int x;
+//         cin >> x;
+//         arr.push_back(x);
+//     }
+//     int count = mergesort(arr, n, 0, n - 1);
+//     cout << count;
+//     return 0;
+// }
+
+// Reverse pairs
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int merge(vector<int> &arr, int n, int low, int mid, int high)
+void merge(vector<int> &arr, int n, int low, int mid, int high)
 {
     vector<int> temp;
     int left = low;
@@ -2163,7 +2235,7 @@ int merge(vector<int> &arr, int n, int low, int mid, int high)
         else
         {
             temp.push_back(arr[right]);
-            count += (mid - left + 1);
+
             right++;
         }
     }
@@ -2181,9 +2253,20 @@ int merge(vector<int> &arr, int n, int low, int mid, int high)
     {
         arr[i] = temp[i - low];
     }
-    return count;
 }
 
+int countpairs(vector<int> &arr, int n, int low, int mid, int high)
+{
+    int right = mid + 1;
+    int count = 0;
+    for (int i = low; i <= mid; i++)
+    {
+        while (right <= high && arr[i] > 2 * arr[right])
+            right++;
+        count += right - (mid + 1);
+    }
+    return count;
+}
 int mergesort(vector<int> &arr, int n, int low, int high)
 {
     int count = 0;
@@ -2193,8 +2276,14 @@ int mergesort(vector<int> &arr, int n, int low, int high)
         return count;
     count += mergesort(arr, n, low, mid);
     count += mergesort(arr, n, mid + 1, high);
-    count += merge(arr, n, low, mid, high);
+    count += countpairs(arr, n, low, mid, high);
+    merge(arr, n, low, mid, high);
     return count;
+}
+int reversepairs(vector<int> &arr)
+{
+    int n = arr.size();
+    return mergesort(arr, n, 0, n - 1);
 }
 
 int main()
@@ -2209,7 +2298,7 @@ int main()
         cin >> x;
         arr.push_back(x);
     }
-    int count = mergesort(arr, n, 0, n - 1);
+    int count = reversepairs(arr);
     cout << count;
     return 0;
 }
