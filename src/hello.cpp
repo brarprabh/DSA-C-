452631ad6d5f12745849067170173b374a736c8c
brarprabh <prabhjotsinghbrar39@gmail.com>
2025-08-30 10:31:02 +0530

    deletion in ll

--- src/hello.cpp
+++ src/hello.cpp
@@ -3633,20 +3633,87 @@
 
 // Linked list
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int data;
+//     Node *next; // node type da ptr banaya
+// public:
+//     Node(int data1, Node *next1) // constructor
+//     {
+//         data = data1;
+//         next = next1;
+//     }
+//     Node(int data1) // constructor
+//     {
+//         data = data1;
+//         next = nullptr;
+//     }
+// };
+
+// Node *convertarrtoll(vector<int> &arr) // convert arr to linked list
+// {
+//     Node *head = new Node(arr[0]); // constructor will be called...    // Always keep head..
+//     Node *mover = head;            // head will always point to first element...
+//     for (int i = 1; i < arr.size(); i++)
+//     {
+//         Node *temp = new Node(arr[i]); // second list will be created
+//         mover->next = temp;            // but the link will be made here..
+//         mover = temp;                  // then move mover to temp..
+//     }
+//     return head;
+// }
+
+// int ifpresent(Node *head, int ele)
+// {
+//     Node *temp = head;
+//     while (temp)
+//     {
+//         if (temp->data == ele)
+//             return 1;
+//         temp = temp->next;
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {1, 2, 3, 4, 5};
+//     // Node *y = new Node(arr[0], nullptr); // Node tahra de ptr nu point krvona
+//     // cout << y << endl;                   // prints the address
+//     // cout << y->data << endl;
+//     // cout << y->next;
+//     Node *head = convertarrtoll(arr);
+//     // cout << head->data;
+
+//     Node *temp = head;
+//     int count = 0;
+//     // while (temp)
+//     // {
+//     //     cout << temp->data << " ";
+//     //     temp = temp->next;
+//     //     count++;
+//     // }
+//     // cout << count;
+//     cout << ifpresent(head, 5);
+//     return 0;
+// }
+
+//  deletion  // head nu hmesha lena
+
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
+class Node
 {
 public:
     int data;
-    Node *next; // node tahara da ptr banaya
+    Node *next;
+
 public:
-    Node(int data1, Node *next1)
-    {
-        data = data1;
-        next = next1;
-    }
     Node(int data1)
     {
         data = data1;
@@ -3654,50 +3721,121 @@ public:
     }
 };
 
-Node *convertarrtoll(vector<int> &arr)
+void printnode(Node *head)
 {
-    Node *head = new Node(arr[0]); // constructor will be called...
-    Node *mover = head;            // head will always point to first element...
+    while (head)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+Node *converttoll(vector<int> arr) // converting to ll  // also we have to return pointer
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
     for (int i = 1; i < arr.size(); i++)
     {
-        Node *temp = new Node(arr[i]); // second list will be created
-        mover->next = temp;            // but the link will be made here..
-        mover = temp;                  // then move mover to temp..
+        Node *temp = new Node(arr[i]);
+        mover->next = temp; // link is made
+        mover = temp;
     }
     return head;
 }
 
-int ifpresent(Node *head, int ele)
+Node *deletehead(Node *head)
 {
+    if (head == NULL)
+        return head;
     Node *temp = head;
-    while (temp)
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+Node *removetail(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+    Node *temp = head;
+    while (temp->next->next != NULL) // Second last element..
     {
-        if (temp->data == ele)
-            return 1;
+        temp = temp->next; // just moving the temp...
+    }
+    delete temp->next; // delete the last element..
+    temp->next = NULL;
+    return head;
+}
+
+Node *removek(Node *head, int k)
+{
+    if (head == NULL)
+        return head;
+    if (k == 1)
+    { // removing first element..
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    Node *temp = head;
+    Node *prev = NULL;
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        if (count == k)
+        {
+            prev->next = prev->next->next; // inbetween toh link remove krta..
+            delete temp;
+            break;
+        }
+        prev = temp;
         temp = temp->next;
     }
-    return 0;
+    return head;
+}
+
+Node *removeEl(Node *head, int el)
+{
+    if (head == NULL)
+        return head;
+    if (head->data == el)
+    { // removing first element..
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    Node *temp = head;
+    Node *prev = NULL;
+    int count = 0;
+    while (temp != NULL)
+    {
+        if (temp->data == el)
+        {
+            prev->next = prev->next->next; // inbetween toh link remove krta..
+            delete temp;
+            break;
+        }
+
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
 }
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4, 5};
-    // Node *y = new Node(arr[0], nullptr); // Node tahra de ptr nu point krvona
-    // cout << y << endl;                   // prints the address
-    // cout << y->data << endl;
-    // cout << y->next;
-    Node *head = convertarrtoll(arr);
-    // cout << head->data;
 
-    Node *temp = head;
-    int count = 0;
-    // while (temp)
-    // {
-    //     cout << temp->data << " ";
-    //     temp = temp->next;
-    //     count++;
-    // }
-    // cout << count;
-    cout << ifpresent(head, 5);
+    vector<int> arr = {1, 2, 3, 4, 5};
+
+    Node *head = converttoll(arr); // head will always be returned by creating linked list
+                                   // head = deletehead(head);
+                                   // head = removetail(head);
+                                   // head = removek(head, 3);
+    head = removeEl(head, 3);
+    printnode(head);
     return 0;
 }
